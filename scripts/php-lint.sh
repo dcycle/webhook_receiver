c714ccb9413208a8c7016ff5ab30df2dade7828b
Albert Albala <albert@mediatribe.net>
2023-07-11 15:13:53 -0400

    Provide a helper script to fix PHP lint errors (#8)

--- scripts/php-lint.sh
+++ scripts/php-lint.sh
@@ -4,12 +4,21 @@
 #
 set -e
 
-echo '=> Linting PHP code.'
-echo 'If you are getting a false negative, use:'
+echo 'Linting PHP files with https://github.com/dcycle/docker-php-lint'
+echo 'If you are getting a false positive, use:'
 echo ''
 echo '// @codingStandardsIgnoreStart'
 echo '...'
 echo '// @codingStandardsIgnoreEnd'
 echo ''
-docker run --rm -v "$(pwd)":/code dcycle/php-lint:3 --extensions=php,module,install,inc --standard=DrupalPractice /code
-docker run --rm -v "$(pwd)":/code dcycle/php-lint:3 --extensions=php,module,install,inc --standard=Drupal /code
+echo 'To automatically fix errors, you can run:'
+echo ''
+echo './scripts/lint-php-fix.sh'
+echo ''
+
+docker run --rm \
+  -v "$(pwd)":/code dcycle/php-lint:3 \
+  --extensions=php,module,install,inc --standard=DrupalPractice /code
+docker run --rm \
+  -v "$(pwd)":/code dcycle/php-lint:3 \
+  --extensions=php,module,install,inc --standard=Drupal /code
